050c91c8e9e3783a6fa9846f85ad452724e0658c
Kevin Silva <kevinsilvaofl@gmail.com>
2024-04-19 15:18:17 -0300

    Initialization of window altered

--- main.py
+++ main.py
@@ -1,8 +1,180 @@
-import flet as ft
+import flet
+from flet import (
+    AppBar,
+    Column,
+    Container,
+    ElevatedButton,
+    InputFilter,
+    MainAxisAlignment,
+    Page,
+    Row,
+    Text,
+    TextField,
+    UserControl,
+)
 
 
-def main(page: ft.page):
-    # Configuring the personalize of page desktop
+class BinaryCalculator(UserControl):
+    def build(self):
+
+        Page.appbar = AppBar(
+            title=Text("Binary Calculator"),
+        )
+
+        # Variable of controls
+        self.txt_hexadecimal = TextField(
+            label="Hexadecimal",
+            width=350,
+            rtl=True,
+            input_filter=InputFilter(regex_string=r"[0-9/aA-hH]"),
+        )  # noqa: F841
+        self.txt_decimal = TextField(
+            label="Decimal",
+            width=350,
+            rtl=True,
+            input_filter=InputFilter(regex_string=r"[0-9]"),
+        )  # noqa: F841
+        self.txt_octal = TextField(
+            label="Octal",
+            width=350,
+            rtl=True,
+            input_filter=InputFilter(regex_string=r"[0-7]"),
+        )  # noqa: F841
+        self.txt_binary = TextField(
+            label="Binary",
+            width=350,
+            rtl=True,
+            input_filter=InputFilter(regex_string=r"[0-1]"),
+        )  # noqa: F841
+
+        return Container(
+            content=Column(
+                controls=[
+                    Row(
+                        controls=[self.txt_hexadecimal],
+                        alignment=MainAxisAlignment.CENTER,
+                    ),
+                    Row(
+                        controls=[self.txt_decimal],
+                        alignment=MainAxisAlignment.CENTER,
+                    ),
+                    Row(controls=[self.txt_octal], alignment=MainAxisAlignment.CENTER),
+                    Row(
+                        controls=[self.txt_binary],
+                        alignment=MainAxisAlignment.CENTER,
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(
+                                " H ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " G ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " F ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                "A/C",
+                                width=80,
+                            ),
+                        ],
+                        alignment=MainAxisAlignment.CENTER,
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(
+                                " E ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " D ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " C ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " B ",
+                                width=80,
+                            ),
+                        ],
+                        alignment=MainAxisAlignment.CENTER,
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(
+                                " A ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " 9 ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " 8 ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " 7 ",
+                                width=80,
+                            ),
+                        ],
+                        alignment=MainAxisAlignment.CENTER,
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(
+                                " 6 ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " 5 ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " 4 ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " 3 ",
+                                width=80,
+                            ),
+                        ],
+                        alignment=MainAxisAlignment.CENTER,
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(
+                                " 2 ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " 1 ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " 0 ",
+                                width=80,
+                            ),
+                            ElevatedButton(
+                                " = ",
+                                width=80,
+                            ),
+                        ],
+                        alignment=MainAxisAlignment.CENTER,
+                    ),
+                ]
+            )
+        )
+
+
+def main(page: Page):
+    # Configuring the personalize of Page desktop
     page.title = "Binary Calculator"
     page.window_height = 600
     page.window_width = 600
@@ -12,161 +184,9 @@ def main(page: ft.page):
     page.window_min_width = 400
     page.window_maximizable = False
 
-    page.appbar = ft.AppBar(
-        title=ft.Text("Binary Calculator"),
-    )
+    calc = BinaryCalculator()
 
-    # Variable of controls
-    txt_hexadecimal = ft.TextField(
-        label="Hexadecimal",
-        width=350,
-        rtl=True,
-        input_filter=ft.InputFilter(regex_string=r"[0-9/aA-hH]"),
-    )  # noqa: F841
-    txt_decimal = ft.TextField(
-        label="Decimal",
-        width=350,
-        rtl=True,
-        input_filter=ft.InputFilter(regex_string=r"[0-9]"),
-    )  # noqa: F841
-    txt_octal = ft.TextField(
-        label="Octal",
-        width=350,
-        rtl=True,
-        input_filter=ft.InputFilter(regex_string=r"[0-7]"),
-    )  # noqa: F841
-    txt_binary = ft.TextField(
-        label="Binary",
-        width=350,
-        rtl=True,
-        input_filter=ft.InputFilter(regex_string=r"[0-1]"),
-    )  # noqa: F841
-
-    page.add(
-        ft.Row(
-            controls=[txt_hexadecimal],
-            alignment=ft.MainAxisAlignment.CENTER,
-        ),
-        ft.Row(
-            controls=[txt_decimal],
-            alignment=ft.MainAxisAlignment.CENTER
-        ),
-        ft.Row(
-            controls=[txt_octal],
-            alignment=ft.MainAxisAlignment.CENTER
-        ),
-        ft.Row(
-            controls=[txt_binary],
-            alignment=ft.MainAxisAlignment.CENTER
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(
-                    " H ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " G ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " F ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    "A/C",
-                    width=80,
-                ),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(
-                    " E ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " D ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " C ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " B ",
-                    width=80,
-                ),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(
-                    " A ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " 9 ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " 8 ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " 7 ",
-                    width=80,
-                ),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(
-                    " 6 ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " 5 ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " 4 ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " 3 ",
-                    width=80,
-                ),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        ),
-        ft.Row(
-            controls=[
-                ft.ElevatedButton(
-                    " 2 ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " 1 ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " 0 ",
-                    width=80,
-                ),
-                ft.ElevatedButton(
-                    " = ",
-                    width=80,
-                ),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        ),
-    ),
-
-    page.update()
+    page.add(calc)
 
 
-ft.app(main)
+flet.app(target=main)
